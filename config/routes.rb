4a6cd967281237ebd78e95055937ed2e544e62df
Farson <wg@toptranslation.com>
2018-06-13 15:16:54 +0200

    add toggle_complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :tasks
+
+  resources :tasks do
+    member do
+      patch :toggle_complete
+    end
+  end
 end
