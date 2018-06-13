b07952c0566a961d7f834f22ee970a4c2b92512e
Farson <wg@toptranslation.com>
2018-06-13 14:26:43 +0200

    add show and destroy actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,18 +2,28 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-  
+
   def create
     @task = Task.create(task_params)
     redirect_to tasks_path
   end
-  
+
   def index
     @tasks = Task.all
   end
- 
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
 private
-  
+
   def task_params
     params.require(:task).permit(:title, :note)
   end
